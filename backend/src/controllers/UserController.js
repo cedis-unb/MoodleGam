846931c0d41ee5f671498d3c99ddd713a202be28
vitordiniz25 <vitordiniz25@gmail.com>
2024-03-30 12:46:31 -0300

    fix returns in user controller

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,61 +1,90 @@
-import User from '../models/User.js' // Assumindo que seu modelo de usuário está em '../models/User.js'
+import bcrypt from 'bcryptjs'
+import UserRepository from '../repositories/UserRepository.js'
+
+const { hash } = bcrypt
 
 const UserController = {
   createUser: async (req, res) => {
     try {
-      const newUser = new User(req.body)
-      const savedUser = await newUser.save()
-      res.status(201).json(savedUser)
+      const { email, name, role, password, status } = req.body
+
+      const userExists = await UserRepository.findByEmail(email)
+
+      if (userExists) {
+        return res.status(400).json({
+          message: 'Email já cadastrado',
+        })
+      }
+
+      const hashedPassword = await hash(password, 8)
+
+      if (status) {
+        const savedUser = await UserRepository.create({
+          email,
+          name,
+          role,
+          password: hashedPassword,
+          status,
+        })
+        return res.status(201).json(savedUser)
+      }
+
+      const savedUser = await UserRepository.create({
+        email,
+        name,
+        role,
+        password: hashedPassword,
+      })
+      return res.status(201).json(savedUser)
     } catch (error) {
-      res.status(400).json({ message: error.message })
+      return res.status(400).json({ message: error.message })
     }
   },
 
   getAllUsers: async (req, res) => {
     try {
-      const users = await User.find()
-      res.status(200).json(users)
+      const users = await UserRepository.findAll()
+      return res.status(200).json(users)
     } catch (error) {
-      res.status(500).json({ message: error.message })
+      return res.status(500).json({ message: error.message })
     }
   },
 
   getUserById: async (req, res) => {
     try {
-      const user = await User.findById(req.params.id)
+      const user = await UserRepository.findById(req.params.id)
       if (user) {
-        res.status(200).json(user)
+        return res.status(200).json(user)
       } else {
-        res.status(404).json({ message: 'Usuário não encontrado' })
+        return res.status(404).json({ message: 'Usuário não encontrado' })
       }
     } catch (error) {
-      res.status(500).json({ message: error.message })
+      return res.status(500).json({ message: error.message })
     }
   },
 
   updateUser: async (req, res) => {
     try {
-      const updatedUser = await User.findByIdAndUpdate(
+      const updatedUser = await UserRepository.updateById(
         req.params.id,
-        req.body,
-        { new: true }
+        req.body
       )
-      res.status(200).json(updatedUser)
+      return res.status(200).json(updatedUser)
     } catch (error) {
-      res.status(400).json({ message: error.message })
+      return res.status(400).json({ message: error.message })
     }
   },
 
   deleteUser: async (req, res) => {
     try {
-      const deletedUser = await User.findByIdAndDelete(req.params.id)
+      const deletedUser = await UserRepository.deleteById(req.params.id)
       if (deletedUser) {
-        res.status(200).json({ message: 'Usuário excluído com sucesso' })
+        return res.status(200).json({ message: 'Usuário excluído com sucesso' })
       } else {
-        res.status(404).json({ message: 'Usuário não encontrado' })
+        return res.status(404).json({ message: 'Usuário não encontrado' })
       }
     } catch (error) {
-      res.status(500).json({ message: error.message })
+      return res.status(500).json({ message: error.message })
     }
   },
 }
