f68f5b248c45f8cfd192909ea417ee3014eff7c6
AntonioRangelC <antonio.rangel.02@gmail.com>
2024-04-01 22:14:01 -0300

    Alteração no componente de botão

--- FrontEnd/moodlegam-front-end/src/app/components/Button/index.js
+++ FrontEnd/moodlegam-front-end/src/app/components/Button/index.js
@@ -1,10 +1,10 @@
 import "../Button/style.css"
 
-export default function Button({ type, text, ...props }){
+export default function Button({ type, text, onClick, ...props }){
 
 
     return (
-        <button className="button-component"type={type}>{text}</button>
+        <button className="button-component"type={type} onClick={onClick}>{text}</button>
 
     );
 }
