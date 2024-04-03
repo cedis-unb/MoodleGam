83e0ccbee3430bd965633e19aaa8226b6c3e317e
AntonioRangelC <antonio.rangel.02@gmail.com>
2024-04-03 10:30:09 -0300

    Conectando a página de cadastro com o backend

--- FrontEnd/moodlegam-front-end/src/app/pages/registerUserPage/page.js
+++ FrontEnd/moodlegam-front-end/src/app/pages/registerUserPage/page.js
@@ -11,12 +11,19 @@ export default function RegisterUserPage(){
     
     const [user, setUser] = useState({
         role:"user",
-        username: "",
+        name: "",
         email: "",
         password: "",
         status:"active"
         
     })
+
+    const apiKey = '276a6f1b4611ef755a3f4fb5ca974367'
+
+    const axiosInstance = axios.create({
+        baseURL: 'http://localhost:8080',
+        
+    });
 
     const [confirmPassword, setConfirmPassword] = useState('')
 
@@ -24,41 +31,72 @@ export default function RegisterUserPage(){
 
     const [errorText, setErrorText] = useState('')
 
-
-    const handleSubmit = async () => {
+    function validateUserData(e){
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(user.password !== confirmPassword){
-            console.log("Senhas diferentes")
+        if(!user.email || !user.name || !user.password || !confirmPassword){
+            setIsValid(false)
+            setErrorText("Os campos estão vazios, preencha-os")
+            e.preventDefault()
+            return false
+        }
+        else if(user.password !== confirmPassword){
+            console.log(`Senha são diferentes ${user.password} | ${confirmPassword}`)
             setIsValid(false)
             setErrorText("As senhas inseridas são diferentes")
+            e.preventDefault()
+            return false
         }
-        
-        if(!regex.test(user.email)){
+        else if(!regex.test(user.email)){
             setIsValid(false)
             setErrorText("E-mail em formato inválido")
+            e.preventDefault()
+            return false
         }
         else{
             setIsValid(true)
+            return true
         }
-        try {
-            const response = await axios.post('/api/usuarios', user);
+    }
+    const handleSubmit = async e => {
         
-            if (response.status === 200) {
-              // Cadastro bem-sucedido
-              console.log('Usuário cadastrado:', response.data);
-            } else {
-              // Tratar erros de requisição
-              console.error('Erro ao cadastrar usuário:', response.statusText);
+        
+        if(validateUserData(e)){
+            
+            try {
+                console.log(`user ${user.status} ${user.email} ${user.role} ${user.name}`)
+    
+                const response = await axiosInstance.post(
+                    '/users', 
+                    user,
+                    {
+                        headers: {
+                            'x-api-key': `${apiKey}`
+                        }
+                    }
+                    
+                )
+                .then(res =>{
+                    console.log(res)
+                }).catch(err =>{
+                    console.log(err)
+                });
+            
+                if (response.status === 200) {
+                  // Cadastro bem-sucedido
+                  console.log('Usuário cadastrado:', response.data);
+                } else {
+                  // Tratar erros de requisição
+                  console.error('Erro ao cadastrar usuário:', response.statusText);
+                }
+            } catch (error) {
+                // Tratar erros de rede
+                console.error('Erro de rede:', error.message);
             }
-          } catch (error) {
-            // Tratar erros de rede
-            console.error('Erro de rede:', error.message);
-          }
+        }
+
+        
     }
 
-    const teste = () =>{
-        setErrorText("As senhas inseridas são diferentes")
-    }
 
     return(
         <div className="background-register-user-page">
@@ -68,10 +106,10 @@ export default function RegisterUserPage(){
                     <input 
                         type="text" 
                         placeholder="Nome completo"
-                        onChange={(e) => setUser({...user, username: e.target.value})}
+                        onChange={(e) => setUser({...user, name: e.target.value})}
                     ></input>
                     <input 
-                        type="password" 
+                        type="email" 
                         placeholder="E-mail"
                         onChange={(e) => setUser({...user, email: e.target.value})}
                     ></input>
@@ -99,8 +137,8 @@ export default function RegisterUserPage(){
                 <div className="button-wrapper-register-user">
                     <Button
                         text="Finalizar cadastro"
-                        type="button"
-                        onClick={teste}
+                        type="submit"
+                        onClick={handleSubmit}
                     />
                 </div>
             </form>
