cb7fed9ef0fd1eb3b39627071cda9b3826ef7468
AntonioRangelC <antonio.rangel.02@gmail.com>
2024-04-05 00:24:08 -0300

    Adicionando conexão do front com o back na página de login

--- FrontEnd/moodlegam-front-end/src/app/pages/loginPage/page.js
+++ FrontEnd/moodlegam-front-end/src/app/pages/loginPage/page.js
@@ -1,10 +1,56 @@
+"use client";
 import "../loginPage/style.css";
 import Image from "next/image";
 import Button from "../../components/Button/index";
+import React, { useEffect, useState } from "react";
+import dotenv from 'dotenv'
+import { useRouter } from 'next/navigation';
+import { axiosInstance } from "@/app/config/config";
+dotenv.config()
 
 export default function LoginPage(){
+    const apiKey = '276a6f1b4611ef755a3f4fb5ca974367'
+    
 
+    const router = useRouter();
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorText, setErrorText] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        try {
+            // Enviar os dados do formulário para o backend
+            const response = await axiosInstance.post(
+                '/sessions', 
+                {
+                    email,
+                    password
+                },
+                {
+                    headers: {
+                        'x-api-key': `${apiKey}`
+                    }
+                }
+        );
+
+            // Verificar se o login foi bem-sucedido
+            if (response.status === 200) {
+                // Armazenar o token no localStorage
+                localStorage.setItem('token', response.data.token);
+                
+               console.log("Login efetuado")
+            } 
+            
+
+        } catch (error) {
+            console.error('Erro ao fazer login:', error.response.data.message);
+            setErrorText(error.response.data.message);
+            
+        }
+    };
 
     return (
         <div className="background-login-page">
@@ -12,9 +58,20 @@ export default function LoginPage(){
             <div className="login-container">
                 <p>Faça Login no <span id='orange-moodle'>Moodle</span>Gam</p>
 
-                <form>
-                    <input type="text" placeholder="E-mail"></input>
-                    <input type="password" placeholder="Senha"></input>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder="E-mail"
+                        onChange={(e) => setEmail(e.target.value)}
+                    
+                    ></input>
+
+                    <input 
+                        type="password" 
+                        placeholder="Senha"
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                    <span id="error-text-login">{errorText}</span>
                     <a>Esqueci minha senha</a>
                     <a>Fazer cadastro</a>
                     <div className="button-login-wrapper">
