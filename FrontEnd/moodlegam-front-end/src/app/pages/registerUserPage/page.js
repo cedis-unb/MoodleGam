0c8a79b4e7523f3b60fbc43d31b9d79a238f954e
AntonioRangelC <antonio.rangel.02@gmail.com>
2024-04-05 00:23:23 -0300

    Adicionando modal na página de cadastro

--- FrontEnd/moodlegam-front-end/src/app/pages/registerUserPage/page.js
+++ FrontEnd/moodlegam-front-end/src/app/pages/registerUserPage/page.js
@@ -1,14 +1,15 @@
 "use client";
 import "./style.css"
 import Button from "@/app/components/Button";
+import Modal from "@/app/components/Modal"
 import {useRouter} from "next/navigation";
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-
-
+import {axiosInstance} from '../../config/config'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export default function RegisterUserPage(){
-    
     const [user, setUser] = useState({
         role:"user",
         name: "",
@@ -19,18 +20,12 @@ export default function RegisterUserPage(){
     })
 
     const apiKey = '276a6f1b4611ef755a3f4fb5ca974367'
-
-    const axiosInstance = axios.create({
-        baseURL: 'http://localhost:8080',
-        
-    });
-
     const [confirmPassword, setConfirmPassword] = useState('')
-
+    const [modalOpen, setModalOpen] = useState(false)
     const [isValid, setIsValid] = useState(true)
 
     const [errorText, setErrorText] = useState('')
-
+    const router = useRouter();
     function validateUserData(e){
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if(!user.email || !user.name || !user.password || !confirmPassword){
@@ -58,12 +53,12 @@ export default function RegisterUserPage(){
         }
     }
     const handleSubmit = async e => {
-        
+        e.preventDefault()
         
         if(validateUserData(e)){
             
             try {
-                console.log(`user ${user.status} ${user.email} ${user.role} ${user.name}`)
+                console.log(`user ${apiKey}`)
     
                 const response = await axiosInstance.post(
                     '/users', 
@@ -75,75 +70,93 @@ export default function RegisterUserPage(){
                     }
                     
                 )
-                .then(res =>{
-                    console.log(res)
-                }).catch(err =>{
-                    console.log(err)
-                });
+               
             
-                if (response.status === 200) {
+                if (response.status === 201) {
                   // Cadastro bem-sucedido
                   console.log('Usuário cadastrado:', response.data);
+                  setErrorText('')
+                  setModalOpen(true)
+                  
                 } else {
                   // Tratar erros de requisição
                   console.error('Erro ao cadastrar usuário:', response.statusText);
                 }
             } catch (error) {
                 // Tratar erros de rede
-                console.error('Erro de rede:', error.message);
+                console.error('Erro de rede:', error.response);
+                setErrorText(error.response.data.message)
             }
         }
 
         
     }
 
+    const redirectToLoginPage = () => {
+        
+        router.push('/pages/loginPage'); // Redireciona para a página de login
+    };
 
     return(
-        <div className="background-register-user-page">
-            <h1>Cadastro de novo usuário</h1>
-            <form className="fields-register-user" onSubmit={handleSubmit}>
-                <div className="input-row-register-user">
-                    <input 
-                        type="text" 
-                        placeholder="Nome completo"
-                        onChange={(e) => setUser({...user, name: e.target.value})}
-                    ></input>
-                    <input 
-                        type="email" 
-                        placeholder="E-mail"
-                        onChange={(e) => setUser({...user, email: e.target.value})}
-                    ></input>
-                </div>
+        <>
+            {modalOpen && (
 
-                <div className="input-row-register-user">
-                    <input 
-                        type="password" 
-                        placeholder="Senha"
-                        onChange={(e) => setUser({...user, password: e.target.value})}
-                    >
-                    </input>
-                    <input 
-                        type="password" 
-                        placeholder="Confirmar senha"
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    ></input>
-                </div>
-                <div className="error-register-wrapper">
-                    <span id="error-text-register-user">{errorText}</span>
+                <Modal
+                    bodyText="Usuário cadastrado com sucesso !"
+                    buttonText="Voltar para tela de login"
+                    onConfirm={redirectToLoginPage}
+                />
 
-                </div>
-                
-                
-                <div className="button-wrapper-register-user">
-                    <Button
-                        text="Finalizar cadastro"
-                        type="submit"
-                        onClick={handleSubmit}
-                    />
-                </div>
-            </form>
+
+            )}
             
+        
+            <div className="background-register-user-page">
+                <h1>Cadastro de novo usuário</h1>
+                <form className="fields-register-user" onSubmit={handleSubmit}>
+                    <div className="input-row-register-user">
+                        <input 
+                            type="text" 
+                            placeholder="Nome completo"
+                            onChange={(e) => setUser({...user, name: e.target.value})}
+                        ></input>
+                        <input 
+                            type="email" 
+                            placeholder="E-mail"
+                            onChange={(e) => setUser({...user, email: e.target.value})}
+                        ></input>
+                    </div>
 
-        </div>
+                    <div className="input-row-register-user">
+                        <input 
+                            type="password" 
+                            placeholder="Senha"
+                            onChange={(e) => setUser({...user, password: e.target.value})}
+                        >
+                        </input>
+                        <input 
+                            type="password" 
+                            placeholder="Confirmar senha"
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        ></input>
+                    </div>
+                    <div className="error-register-wrapper">
+                        <span id="error-text-register-user">{errorText}</span>
+
+                    </div>
+                    
+                    
+                    <div className="button-wrapper-register-user">
+                        <Button
+                            text="Finalizar cadastro"
+                            type="submit"
+                            onClick={handleSubmit}
+                        />
+                    </div>
+                </form>
+                
+
+            </div>
+        </>
     );
 }
