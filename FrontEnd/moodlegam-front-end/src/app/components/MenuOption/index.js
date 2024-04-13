9bda9db9cc75009f760d178f8b8a9ff32bbb3067
AntonioRangelC <antonio.rangel.02@gmail.com>
2024-04-13 10:11:18 -0300

    adicionando página de menu de disciplina

--- FrontEnd/moodlegam-front-end/src/app/components/MenuOption/index.js
+++ FrontEnd/moodlegam-front-end/src/app/components/MenuOption/index.js
@@ -1,10 +1,17 @@
+"use client";
 import "./style.css"
+import {useRouter} from "next/navigation";
 
 export default function MenuOption(props){
+    const router = useRouter();
 
+    const redirect = () => {
+        
+        router.push(`/pages/${props.redirect}`);
+    }
 
     return (
-        <a className="option-container">
+        <a className="option-container" onClick={redirect}>
             <div className="option-icon-conteiner">
                 
                 {props.children}
--- FrontEnd/moodlegam-front-end/src/app/pages/subject/page.js
+++ FrontEnd/moodlegam-front-end/src/app/pages/subject/page.js
@@ -4,65 +4,41 @@ import MenuOption from "@/app/components/MenuOption"
 import Image from "next/image";
 import "./style.css"
 
-export default function Homepage(){
+export default function Subject(){
 
-    
+
 
 
     return (
         <Background>
 
-            <div className="homepage-option">
-                
+            <div className="subject-option">
+
                 <MenuOption
                     title="Disciplina"
                     text="Crie e edite Disciplinas, além de ver as que já foram criadas. Ao criar uma Disciplina é possível adicionar turmas para mesma. "
-                    redirect="subject"
                 >
                     <Image 
-                        src="/img/books.svg"
+                        src="/img/plus.svg"
                         width={50}
                         height={50}
                         unoptimized={true}
                     />
                 </MenuOption>
-                
 
                 <MenuOption
                     title="Conteúdos"
                     text="Veja conteúdos sobre Gamificação, além de tutoriais sobre como utilizar o Moodle"
                 >
                     <Image 
-                        src="/img/content.svg"
-                        width={40}
-                        height={40}
-                        unoptimized={true}
-                    />
-                </MenuOption>
-
-                <MenuOption
-                    title="Sobre nós"
-                    text="Saiba  sobre os criadores do MoodleGam"
-                >
-                    <Image 
-                        src="/img/info.svg"
+                        src="/img/list.svg"
                         width={50}
                         height={50}
                         unoptimized={true}
                     />
                 </MenuOption>
 
-                <MenuOption
-                    title="Ajuda"
-                    text="Instruções sobre como utilizar o MoodleGam, e dúvidas mais frequentes"
-                >
-                    <Image 
-                        src="/img/help.svg"
-                        width={50}
-                        height={50}
-                        unoptimized={true}
-                    />
-                </MenuOption>
+               
 
             </div>
             
