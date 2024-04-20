6dd44affabaedc94ff71b7cbc0616cd9018eb356
vitordiniz25 <vitordiniz25@gmail.com>
2024-04-20 13:38:21 -0300

    create core drive schema

---
+++ backend/src/models/CoreDrive.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const coreDriveSchema = new Schema(
+  {
+    role: {
+      type: String,
+      required: true,
+      enum: ['user', 'admin'],
+    },
+    coreDriveName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    motivation: {
+      type: String,
+      required: true,
+      enum: ['intrinsic', 'extrinsic'],
+    },
+    hat: {
+      type: String,
+      required: true,
+      enum: ['black', 'white'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const CoreDrive = mongoose.model('CoreDrive', coreDriveSchema)
+
+export default CoreDrive
