fbc0c2cfd015abba03772f10fddcd28bece42270
vitordiniz25 <vitordiniz25@gmail.com>
2024-04-21 19:32:03 -0300

    remove authentication middleware from plugin routes

--- backend/src/routes/PluginRoutes.js
+++ backend/src/routes/PluginRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import PluginController from '../controllers/PluginController.js'
-import isAuthenticated from '../middlewares/isAuthenticated.js'
 
 const pluginRouter = express.Router()
 
-pluginRouter.get('/', isAuthenticated, PluginController.getAllPlugins)
-pluginRouter.get('/:id', isAuthenticated, PluginController.getPluginById)
-pluginRouter.post('/', isAuthenticated, PluginController.createPlugin)
-pluginRouter.delete('/:id', isAuthenticated, PluginController.deletePlugin)
-pluginRouter.put('/:id', isAuthenticated, PluginController.updatePlugin)
+pluginRouter.get('/', PluginController.getAllPlugins)
+pluginRouter.get('/:id', PluginController.getPluginById)
+pluginRouter.post('/', PluginController.createPlugin)
+pluginRouter.delete('/:id', PluginController.deletePlugin)
+pluginRouter.put('/:id', PluginController.updatePlugin)
+pluginRouter.patch('/:pluginId', PluginController.addTechniqueToPlugin)
 
 export default pluginRouter
