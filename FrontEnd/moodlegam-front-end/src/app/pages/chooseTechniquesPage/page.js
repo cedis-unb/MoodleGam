bc757bc984e5329e51ef6a17d9d036f4d65bd300
AntonioRangelC <antonio.rangel.02@gmail.com>
2024-04-28 17:12:56 -0300

    Adicionando checkboxes e lógica de risco de implementação na página de escolher técnicas

--- FrontEnd/moodlegam-front-end/src/app/pages/chooseTechniquesPage/page.js
+++ FrontEnd/moodlegam-front-end/src/app/pages/chooseTechniquesPage/page.js
@@ -1,13 +1,90 @@
 
+"use client"
 import Background from "../../components/Background";
 import Image from "next/image";
 import "./style.css"
 import "../subjectDetailsPage/style.css"
+import { useEffect, useState } from "react";
 
 export default function ChooseTechniquePage(){
 
+    const [coreDrive, setCoreDrive] = useState({
+        nome: "Core Drive 1",
+        id: 1,
+        tecnicas: [
+            { id: 1, nome: "Preguiça como Status Quo" },
+            { id: 2, nome: "Sorte de Principiante" },
+            { id: 3, nome: "Pontos" },
+            { id: 4, nome: "Preguiça como Status Quo" },
+            { id: 5, nome: "Preguiça como Status Quo" },
+            { id: 6, nome: "Preguiça como Status Quo" },
+            { id: 7, nome: "Preguiça como Status Quo" },
+            { id: 8, nome: "Preguiça como Status Quo" },
+            { id: 9, nome: "Preguiça como Status Quo" },
+            { id: 10, nome: "Preguiça como Status Quo" },
+            { id: 11, nome: "Preguiça como Status Quo" }
+        ]
+    });
 
+    const recommendedQuantity = 10
 
+    const [techniqueQuantity, setTechniqueQuantity] = useState(0)
+    const [risk, setRisk] = useState("Baixa")
+    function addTechnique(){
+        setTechniqueQuantity(techniqueQuantity + 1)
+    }
+
+    function subtractTechnique(){
+        setTechniqueQuantity(techniqueQuantity - 1)
+    }
+
+    function handleCheckboxChange(techniqueId, isChecked) {
+        if (isChecked) {
+            addTechnique();
+        } else {
+            subtractTechnique();
+        }
+
+        updateRisk()
+        
+    }
+
+    function updateRisk(){
+        if(techniqueQuantity < recommendedQuantity){
+            setRisk("Baixa")
+        }
+        else if(techniqueQuantity == recommendedQuantity){
+            setRisk("Moderada")
+        }
+        else if(techniqueQuantity > recommendedQuantity){
+            setRisk("Alta")
+        }
+    }
+
+    function getRiskLevel(techniqueQuantity) {
+        if (techniqueQuantity > recommendedQuantity) {
+            return "Alta";
+        } else if (techniqueQuantity === recommendedQuantity) {
+            return "Moderada";
+        } else {
+            return "Baixa";
+        }
+    }
+
+    function getRiskColor(riskLevel){
+        if(riskLevel === "Alta"){
+            return "#DC3545"
+        }
+        else if(riskLevel === "Moderada"){
+            return "#FD7E14"
+        }
+        else {
+            return "#28A745"
+        }
+    }
+
+    const riskLevel = getRiskLevel(techniqueQuantity);
+    const riskColor = getRiskColor(riskLevel)
 
     return (
         <Background>
@@ -40,11 +117,11 @@ export default function ChooseTechniquePage(){
                     </div>
                     <div className="technique-quantity">
                         <div className="technique-quantity-text">
-                            <span>Quantidade de técnicas escolhidas: <span className="font-bold">1</span></span>
+                            <span>Quantidade de técnicas escolhidas: <span className="font-bold">{techniqueQuantity}</span></span>
                         </div>
                         
                         <div className="technique-quantity-text">
-                            <span>Dificuldade de implementação: <span className="font-bold">Baixa</span></span>
+                            <span>Dificuldade de implementação: <span style={{color: riskColor, fontWeight: 'bold'}}>{riskLevel}</span></span>
                         </div>
                         
                     </div>
@@ -59,38 +136,30 @@ export default function ChooseTechniquePage(){
                     </div>
 
                     <div className="core-drive-techniques">
-                        <div className="technique-box">
-                            <span>Narrativa</span>
-                        </div>
 
-                        <div className="technique-box">
-                            <span>Narrativa</span>
-                        </div>
+                        {coreDrive && 
+                            coreDrive.tecnicas.map((technique, index) => (
 
-                        <div className="technique-box">
-                            <span>Narrativa</span>
-                        </div>
+                            <div className="choose-technique-box">
+                                <input 
+                                    type="checkbox" 
+                                    id={`technique-${technique.id}`} 
+                                    onChange={(e) => handleCheckboxChange(technique.id, e.target.checked)}
+                                
+                                />
 
-                        <div className="technique-box">
-                            <span>Filho do destino</span>
-                        </div>
+                                <label htmlFor={`technique-${technique.id}`}>
+                                    {technique.nome}
+                                </label>
 
-                        <div className="technique-box">
-                            <span>Narrativa</span>
-                        </div>
+                            </div>
+                                
+                            
+                            ))
+                        }
+                        
 
-                        <div className="technique-box">
-                            <span>Narrativa</span>
-                        </div>
-
-                        <div className="technique-box">
-                            <span>Narrativa</span>
-                        </div>
-
-                        <div className="technique-box">
-                            <span>Filho do destino</span>
-                        </div>
-
+                       
                     </div>
                 </div>
 
