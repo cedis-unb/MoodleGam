92f97a03d4b1b0eda223aff706eafbed3855c63e
AntonioRangelC <antonio.rangel.02@gmail.com>
2024-08-05 13:02:46 -0300

    configurando repo para rodar localmente

--- FrontEnd/moodlegam-front-end/src/app/config/config.js
+++ FrontEnd/moodlegam-front-end/src/app/config/config.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import dotenv from 'dotenv'
 dotenv.config()
-const NEXT_PUBLIC_BACKEND_ADDRESS = process.env.NEXT_PUBLIC_BACKEND_ADDRESS
+const PORT = process.env.PORT || 8080
+
 export const axiosInstance = axios.create({
-    baseURL: NEXT_PUBLIC_BACKEND_ADDRESS,
+    baseURL: `http://localhost:${PORT}`,
 });
 
 
